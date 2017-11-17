f181d84b0812c58e1c5b83cdda59c18229ab9b6e
Clemente <clementem50348@students.laalliance.org>
2017-11-17 17:40:40 +0000

    Done.

---
+++ countries.js
@@ -0,0 +1,7 @@
+var countries = [
+    "Chad", "Cuba", "Greenland", "Iraq", "Mali", "Oman", "India"
+    ]
+countries.push("Fiji")
+countries.splice(3, 0, "Iran")
+countries.pop("India")
+console.log(countries)
